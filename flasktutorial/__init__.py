ce2b5f6092630e312b10b4b25bfc0a81c949a90c
manoj-pun <manojpun840@gmail.com>
2026-02-04 11:22:57 +0545

    feat: email and password reset

--- flasktutorial/__init__.py
+++ flasktutorial/__init__.py
@@ -1,8 +1,12 @@
+import os
 from flask import Flask
-
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Create a new Flask web application
 # 'app' is the variable that stores our Flask app object
@@ -21,8 +25,18 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "info"
+app.config["MAIL_SERVER"] = "smtp.googlemail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("EMAIL_USER")
+app.config["MAIL_PASSWORD"] = os.environ.get("EMAIL_PASS")
+mail = Mail(app)
 
 from flasktutorial.models import User
+
+# print("MAIL USER:", app.config["MAIL_USERNAME"])
+# print("MAIL PASS LENGTH:", len(app.config["MAIL_PASSWORD"]) if app.config["MAIL_PASSWORD"] else None)
+
 
 @login_manager.user_loader
 def load_user(user_id):
